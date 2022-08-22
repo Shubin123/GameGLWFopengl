afa8d2a4f0e377b06b93a0347c92c7501f40efa9
shubin123 <44554264+Shubin123@users.noreply.github.com>
2022-08-22 02:23:58 -0400

    added mouse movement
    hack implementation of mouse movement when shift is pressed

---
+++ source/example_glfw_opengl3/player.cpp
@@ -0,0 +1,86 @@
+#include "player.h"
+
+
+
+// to print
+#include "iostream"
+using namespace std;
+
+// for timer
+#include <chrono>
+#include <thread>
+
+using namespace this_thread;     // sleep_for, sleep_until
+using namespace chrono_literals; // ns, us, ms, s, h, etc.
+using chrono::system_clock;
+using chrono::duration;
+
+
+/// <summary>
+///  decrementf takes float pointer and decrements
+///  incrementf ...
+///
+///  toggleB toggle wether in game : use timer 
+/// </summary>
+/// <param name="f"></param>
+void incrementf(float* f, float factor = 1) {
+    *f += .1f * factor;
+}
+void decrementf(float* f, float factor = 1) {
+    *f -= .1f * factor;
+}
+void toggleB(GLFWwindow* window, bool* b, duration<double> tsecs) {
+    GLint windowWidth, windowHeight;
+    glfwGetWindowSize(window, &windowWidth, &windowHeight);
+    glfwSetCursorPos(window, windowWidth/2, windowHeight/2); // min x and min y
+    glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_NORMAL);
+
+    sleep_for(10ns);
+    sleep_until(system_clock::now() + tsecs);
+        if (*b) {
+            *b = 0;
+        }
+        else {
+            *b = 1;
+        }
+}
+
+
+
+void processInput(GLFWwindow* window, float* z, float* x, float* y, bool* togglemouse) {
+
+    // keyboard
+    if (glfwGetKey(window, GLFW_KEY_W)) {
+        cout << "w pressed" << endl;
+        incrementf(z);
+    }
+    if (glfwGetKey(window, GLFW_KEY_S)) {
+        cout << "s pressed" << endl;
+        decrementf(z);
+    }
+    if (glfwGetKey(window, GLFW_KEY_A)) {
+        cout << "a pressed" << endl;
+        incrementf(x);
+    }
+    if (glfwGetKey(window, GLFW_KEY_D)) {
+        cout << "d pressed" << endl;
+        decrementf(x);
+    }
+    if (glfwGetKey(window, GLFW_KEY_SPACE)) {
+        cout << "space pressed" <<  *y << endl;
+        decrementf(y, 2);
+    }
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE)) {
+        glfwSetWindowShouldClose(window, GLFW_TRUE);
+    }
+    if (glfwGetKey(window, GLFW_KEY_LEFT_SHIFT)) {
+        toggleB(window, togglemouse, .5s);
+        if (*togglemouse) {
+            cout << "mouse hidden" << endl;
+        }
+        else {
+            cout << "mouse shown" << endl;
+        }
+    }
+
+}
---
+++ source/example_glfw_opengl3/rect.cpp
@@ -0,0 +1,94 @@
+#include "rect.h"
+#include "math.h"
+
+#include "iostream"
+using namespace std;
+
+
+void perspectiveGL(GLdouble fovY, GLdouble aspect, GLdouble zNear, GLdouble zFar)
+{
+    const GLdouble pi = 3.1415926535897932384626433832795;
+    GLdouble fW, fH;
+
+    //fH = tan( (fovY / 2) / 180 * pi ) * zNear;
+    fH = tan(fovY / 360 * pi) * zNear;
+    fW = fH * aspect;
+
+    glFrustum(-fW, fW, -fH, fH, zNear, zFar);
+}
+
+void drawRect() {
+    GLfloat vertices[] =
+    {
+        -1, -1, -1,   -1, -1,  1,   -1,  1,  1,   -1,  1, -1,
+        1, -1, -1,    1, -1,  1,    1,  1,  1,    1,  1, -1,
+        -1, -1, -1,   -1, -1,  1,    1, -1,  1,    1, -1, -1,
+        -1,  1, -1,   -1,  1,  1,    1,  1,  1,    1,  1, -1,
+        -1, -1, -1,   -1,  1, -1,    1,  1, -1,    1, -1, -1,
+        -1, -1,  1,   -1,  1,  1,    1,  1,  1,    1, -1,  1
+    };
+
+    GLfloat colors[] =
+    {
+        0, 0, 0,   0, 0, 1,   0, 1, 1,   0, 1, 0,
+        1, 0, 0,   1, 0, 1,   1, 1, 1,   1, 1, 0,
+        0, 0, 0,   0, 0, 1,   1, 0, 1,   1, 0, 0,
+        0, 1, 0,   0, 1, 1,   1, 1, 1,   1, 1, 0,
+        0, 0, 0,   0, 1, 0,   1, 1, 0,   1, 0, 0,
+        0, 0, 1,   0, 1, 1,   1, 1, 1,   1, 0, 1
+    };
+
+    static float alpha = 0;
+    //attempt to rotate cube
+    glRotatef(alpha, 0, 1, 0);
+
+    /* We have a color array and a vertex array */
+    glEnableClientState(GL_VERTEX_ARRAY);
+    glEnableClientState(GL_COLOR_ARRAY);
+    glVertexPointer(3, GL_FLOAT, 0, vertices);
+    glColorPointer(3, GL_FLOAT, 0, colors);
+
+    /* Send data : 24 vertices */
+    glDrawArrays(GL_QUADS, 0, 24);
+
+    /* Cleanup states */
+    glDisableClientState(GL_COLOR_ARRAY);
+    glDisableClientState(GL_VERTEX_ARRAY);
+    alpha += 1;
+}
+
+void translate(float playerx, float playery, float playerz) {
+    glTranslatef(0 + playerx, playery, -5 + playerz);
+}
+void rotate(float playerrotx, float playerroty, float playerrotz, double mousex, double mousey) {
+    glRotated(mousex, playerrotx, playerroty, playerrotz);
+    glRotatef(mousex, playerrotx, playerroty + mousex, playerrotz);
+    glRotatef(mousey, playerrotx + mousey, playerroty, playerrotz);
+}
+void display(GLFWwindow* window, float floater, float playerz, float playerx, float playery, float playerrotx, float playerroty, float playerrotz, double mousex, double mousey) {
+    
+    // Scale to window size
+    GLint windowWidth, windowHeight;
+    glfwGetWindowSize(window, &windowWidth, &windowHeight);
+    glViewport(0, 0, windowWidth, windowHeight);
+
+    // Draw stuff
+    glClearColor(0.0, 0.8, 0.3, 1.0);
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+    glMatrixMode(GL_PROJECTION_MATRIX);
+    glLoadIdentity();
+    perspectiveGL(60, (double)windowWidth / (double)windowHeight, 0.1, 100);
+
+    glMatrixMode(GL_MODELVIEW_MATRIX);
+    
+    rotate(playerrotx, playerroty, playerrotz, mousex, mousey);
+    translate(playerx, playery, playerz);
+
+
+    drawRect();
+    glfwSwapBuffers(window);
+
+    // Update Screen
+
+}
